4d713687ba9fe8acd5e58932e599504dc4db5c8b
Stefano Strippoli <stefano.g.strippoli@gmail.com>
2024-11-13 22:26:51 -0400

    Fixed broken external urls

--- rm_api.php
+++ rm_api.php
@@ -5,7 +5,7 @@
  * Version:     1.0.0
  * Author:      Stefano Strippoli
  * License:     GPLv2 or later
- * License URI: http:/* www.gnu.org/licenses/gpl-2.0.html
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: RM_API
  */
 
