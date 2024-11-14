36ad872dd4e437fbebf8aa86db8e8913bc4c9303
Stefano Strippoli <stefano.g.strippoli@gmail.com>
2024-11-13 21:35:35 -0400

    Added media queries for responsive form, fixed format con comments for easier reading.

--- inc/form.php
+++ inc/form.php
@@ -1,5 +1,5 @@
 <?php
-//Callback for the shortcode, prints the api script of recaptcha on detection, which is detected if the two needed keys are set up on the settings page.
+/* Callback for the shortcode, prints the api script of recaptcha on detection, which is detected if the two needed keys are set up on the settings page.*/
 function rm_shortcode($atts){
   $atts = shortcode_atts(array(
     'title' => 'Rick & Morty Search API',
@@ -14,25 +14,24 @@ function rm_shortcode($atts){
   }
 
   $chars = rm_build_results( rm_get_all_characters() );
-  // Form template and replace data
+  /* Form template and replace data */
   $output .= file_get_contents(RM_DIR . 'templates/search_bar.html');
   $output = str_replace('{{title}}', $atts['title'], $output);
   $output = str_replace('{{dataRecap}}', $data_recap, $output);
   $output = str_replace('{{chars}}', $chars, $output);
   return $output;
 }
-add_shortcode('rm_shortcode', 'rm_shortcode'); //register the shortcode
+add_shortcode('rm_shortcode', 'rm_shortcode'); 
 
-//Callback for the ajax call which gets data from the form and returns all the search related characters information
+/* Callback for the ajax call which gets data from the form and returns all the search related characters information*/
 function rm_submit_search(){
-  // Nonce verification, nonce defined at rm_api.php
   if ( ! wp_verify_nonce( $_POST['nonce'], 'rm_search_nonce' ) ) {
     wp_send_json_error(["message" => 'No funny business, Morty! *burp*']);
     wp_die();
   }
 
-  //Checks if recaptcha is enabled, meaning it has the two keys set up
-  //then validates the recaptcha by checking the response json from the cURL on recaptcha_validation()
+  /* Checks if recaptcha is enabled, meaning it has the two keys set up
+  then validates the recaptcha by checking the response json from the cURL on recaptcha_validation()*/
   if( rm_detect_recaptcha() ){
     $recaptcha = recaptcha_validation($_POST['token']);
     if ( ! $recaptcha['success'] ){
@@ -41,7 +40,6 @@ function rm_submit_search(){
     }
   }
 
-  //Use custom function to sanitize and return sanitized data.
   $rm_data  = rm_sanitize();
   $query    = rm_build_query($rm_data['name'], $_POST['rm_status'], $rm_data['species'], $rm_data['type'], $_POST['rm_gender']);
   $rm_data  = rm_filter_characters($query);
@@ -53,14 +51,14 @@ function rm_submit_search(){
 }
 
 function rm_submit_search_page(){
-  // Nonce verification, nonce define at rm_api.php
+
   if ( ! wp_verify_nonce( $_POST['nonce'], 'rm_search_page_nonce' ) ) {
     wp_send_json_error(["message" => 'No funny business, Morty! *burp*']);
     wp_die();
   }
 
-  //Checks if recaptcha is enabled, meaning it has the two keys set up
-  //then validates the recaptcha by checking the response json from the cURL on recaptcha_validation()
+  /* Checks if recaptcha is enabled, meaning it has the two keys set up
+  then validates the recaptcha by checking the response json from the cURL on recaptcha_validation()*/
   if( rm_detect_recaptcha() ){
     $recaptcha = recaptcha_validation($_POST['token']);
     if ( ! $recaptcha['success'] ){
--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,6 @@
 <?
-//sanitizes post data and returns sanitized in an array
+/* sanitizes post data and returns sanitized in an array*/
 function rm_sanitize(){
-    // Sanitize data
     $name   = sanitize_text_field($_POST['rm_name']);
     $species = sanitize_text_field($_POST['rm_species']);
     $type   = sanitize_text_field($_POST['rm_type']);
@@ -13,12 +12,12 @@ function rm_sanitize(){
     );
 }
 
-//Callback for the settings page
+/* Callback for the settings page*/
 function rm_settings_page(){
     require(RM_DIR . 'admin/rm_settings_page.php');
 }
 
-//this function builds the query for the filter rm_filter_characters function.
+/* this function builds the query for the filter rm_filter_characters function.*/
 function rm_build_query($name, $status, $specie, $type, $gender){
     $qry = '?';
     if($name) $qry .= 'name='.urlencode($name).'&';
@@ -30,28 +29,28 @@ function rm_build_query($name, $status, $specie, $type, $gender){
     return $qry;
 }
 
-//This one is for usage along with rn_build_query. Simply pass the query with vars
+/* This one is for usage along with rn_build_query. Simply pass the query with vars*/
 function rm_filter_characters($qry_str){
-    $url = 'https://rickandmortyapi.com/api/character/' . $qry_str;
+    $url = 'https:/* rickandmortyapi.com/api/character/' . $qry_str;
     $response = wp_remote_get( $url, ["headers" => ['Content-Type' => 'application/json', 'Accept' => 'application/json']] );
-    if (is_wp_error($response)) return false; // Return false on error
-    return json_decode( wp_remote_retrieve_body($response) , true); // Return decoded body
+    if (is_wp_error($response)) return false; 
+    return json_decode( wp_remote_retrieve_body($response) , true); 
 }
 
-//This one is for pagination, doesn't require anything more than the url
+/* This one is for pagination, doesn't require anything more than the url*/
 function rm_filter_characters_page($url){
     $response = wp_remote_get( $url, ["headers" => ['Content-Type' => 'application/json', 'Accept' => 'application/json']] );
-    if (is_wp_error($response)) return false; // Return false on error
-    return json_decode( wp_remote_retrieve_body($response) , true); // Return decoded body
+    if (is_wp_error($response)) return false; 
+    return json_decode( wp_remote_retrieve_body($response) , true); 
 }
 
-//this gets all characters it's only used for first render.
+/* this gets all characters it's only used for first render.*/
 function rm_get_all_characters(){
-    $response = wp_remote_get( 'https://rickandmortyapi.com/api/character', ["headers" => ['Content-Type' => 'application/json', 'Accept' => 'application/json']] );
-    if (is_wp_error($response)) return false; // Return false on error
-    return json_decode( wp_remote_retrieve_body($response) , true); // Return decoded body
+    $response = wp_remote_get( 'https:/* rickandmortyapi.com/api/character', ["headers" => ['Content-Type' => 'application/json', 'Accept' => 'application/json']] );
+    if (is_wp_error($response)) return false; 
+    return json_decode( wp_remote_retrieve_body($response) , true);
 }
-//this builds all the html that goes inside #rm_results which is the wrapper element.
+/* this builds all the html that goes inside #rm_results which is the wrapper element. */
 function rm_build_results($rm_data){
     $output = '';
     if($rm_data['results']){
@@ -85,25 +84,24 @@ function rm_build_results($rm_data){
       $output .= '</div>';
       return $output;
 }
-//Detects if recaptcha settings on the page have anything.
-//returns a script to add the recaptcha api with the site key to the head
-//used at the form shortcode function rm_shortcode()
+/* Detects if recaptcha settings on the page have anything.
+returns a script to add the recaptcha api with the site key to the head
+used at the form shortcode function rm_shortcode() */
 function rm_detect_recaptcha(){
     if ( !empty(get_option('rm_recaptcha_site_key')) and !empty(get_option('rm_recaptcha_secret_key'))) {
         $output = '<script>';
         $output .= "const siteKey = '" . get_option('rm_recaptcha_site_key') . "';\n";
-        $output .= 'if (siteKey) { const script = document.createElement("script");script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`; document.head.appendChild(script); }';
+        $output .= 'if (siteKey) { const script = document.createElement("script");script.src = `https:/* www.google.com/recaptcha/api.js?render=${siteKey}`; document.head.appendChild(script); }';
         $output .= '</script>';
         return $output;
     }
     return;
 }
 
-//Uses cURL to validate the token from the form with the secret key from the settings page.
+/* Uses cURL to validate the token from the form with the secret key from the settings page.*/
 function recaptcha_validation($token){
-
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+    curl_setopt($ch, CURLOPT_URL, "https:/* www.google.com/recaptcha/api/siteverify");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' =>  get_option('rm_recaptcha_secret_key'), 'response' => $token)));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
